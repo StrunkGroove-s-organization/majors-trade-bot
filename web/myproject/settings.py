c92f558bd4f623afefb6fbce01eebb2215508290
StrunkGroove <jmarkj@mail.ru>
2024-03-06 14:50:42 +0300

    set docker-compose.prod

--- web/myproject/settings.py
+++ web/myproject/settings.py
@@ -15,7 +15,6 @@ EXTENSION_APP = [
     'parsing',
     'count',
     'main',
-    'binance',
     'rest_framework'
 ]
 
@@ -123,6 +122,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
